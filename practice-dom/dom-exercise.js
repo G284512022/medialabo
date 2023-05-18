ed5633e03b00e81050efb9166a4224dc4989e868
G2284512022 <228451@st.takushoku-u.ac.jp>
2023-05-18 14:59:22 +0900

    1

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,8 +14,11 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
-
-
+let h2 = document.querySelector('h2#ex42');
+let p = document.createElement('p');				// 新しい p要素を作成
+p.textContent = '>写真表と都市の緯度経度のページです';
+p.style.color='sesame green';
+h2.insertAdjacentElement('beforeend', p);
 // 練習4-3 写真表作成プログラム
 
 
