4a3319735baaf284d544d391f2744eb71c0ffedd
G2284512022 <228451@st.takushoku-u.ac.jp>
2023-05-25 14:12:43 +0900

    1

---
+++ kazuate2.js
@@ -0,0 +1,35 @@
+let kotae = Math.floor(Math.random()*10) + 1;
+console.log('答え（デバッグ用）: ' + kotae);
+
+let kaisu = 1;
+
+function hantei() {
+    let yoso = 4;
+    document.getElementById("kaisu").textContent = kaisu;
+    document.getElementById("answer").textContent = yoso;
+  
+    if(kaisu < 4){
+        if(kaisu === 3){
+            if(yoso === kotae){
+                document.getElementById("result").textContent = '正解です．おめでとう!';
+                kaisu = 4;
+            }else{
+                document.getElementById("result").textContent = 'まちがい．残念でした答えは' + kotae + 'です．';
+            }
+        }else{
+            if(yoso === kotae){
+                document.getElementById("result").textContent = '正解です．おめでとう!';
+                kaisu = 4;
+            }else if(yoso < kotae){
+                document.getElementById("result").textContent = 'まちがい．答えはもっと大きいですよ';
+            }else{
+                document.getElementById("result").textContent = 'まちがい．答えはもっと小さいですよ';
+            }
+        }
+    }else{
+        document.getElementById("result").textContent = "答えは" + kotae + "でした．すでにゲームは終わっています";
+    }
+    kaisu++;
+}
+
+hantei();
