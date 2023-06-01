d50cedff0142038d71d0f4d71c17b31cc561f489
G2284512022 <228451@st.takushoku-u.ac.jp>
2023-06-01 16:16:04 +0900

    1

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-let b = document.querySelector('#sendRequest');
+let b = document.querySelector('#bt');
 b.addEventListener('click', sendRequest);
 
 
@@ -27,8 +27,6 @@ function showResult(resp) {
     // data をコンソールに出力
     console.log(data);
 
-    // data.x を出力
-    console.log(data.x);
 }
 
 // 通信エラーが発生した時の処理
