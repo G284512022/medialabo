bd06b061b267cf5c60b2369a51fa8544e2d74cc1
G2284512022 <228451@st.takushoku-u.ac.jp>
2023-06-02 23:07:51 +0900

    1

--- project.js
+++ project.js
@@ -27,17 +27,6 @@ function showResult(resp) {
         data = JSON.parse(data);
     }
 
-    // data をコンソールに出力
-    console.log(data);
-    console.log(data.name);
-    console.log(data.coord.lon);
-    console.log(data.coord.lat);
-    console.log(data.weather[0].description);
-    console.log(data.main.temp_min);
-    console.log(data.main.temp_max);
-    console.log(data.main.humidity);
-    console.log(data.wind.speed);
-    console.log(data.wind.deg);
     document.getElementById("name").innerHTML = data.name;
     document.getElementById("coord.lon").innerHTML = data.coord.lon;
     document.getElementById("coord.lat").innerHTML = data.coord.lat;
